f22571cd95c8a9af6da69e73bb3c15912ef7e0bd
ghongatevivek <ghongatevivek1212@gmail.com>
2023-11-18 23:15:10 +0530

    Worked on external css import & google font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
-
-ReactDOM.render(<><h1>Hello World</h1><p>Vivek Ghongate</p> </>,
+const firstName = 'Vivek';
+const lastName = 'Ghongate';
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/100/300";
+ReactDOM.render(
+    <>
+        <h1 className="heading_text">Hello World</h1>
+        <div className='img_div'>
+            <img src= {img1} alt='Sample Image' />
+            <img src= {img2} alt='Sample Image' />
+            <img src= {img3} alt='Sample Image' />
+        </div>
+    </>,
     document.getElementById('root'));
